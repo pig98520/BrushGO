d8931102bde3c792c22fed574e5ad1dfc313107c
Swlabgroup2 <swlabgroup2@gmail.com>
2017-05-07 17:08:30 +0800

    2017/05/07

--- app/src/main/java/com/example/brush/brushgo/Home_Activity.java
+++ app/src/main/java/com/example/brush/brushgo/Home_Activity.java
@@ -1,34 +1,56 @@
 package com.example.brush.brushgo;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-
-import java.util.Set;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by swlab on 2017/5/5.
  */
 
 public class Home_Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-
+    private Button play;
+    private Button stop;
+    private Button next;
+    private Button previous;
+    private MediaPlayer music;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
         processView();
+        processControl();
     }
 
     private void processView() {
         NavigationView navigateionView=(NavigationView) findViewById(R.id.nav_home);
         navigateionView.setNavigationItemSelectedListener(Home_Activity.this);
+        play=(Button) findViewById(R.id.btn_play);
+        stop=(Button) findViewById(R.id.btn_pause);
+        next=(Button) findViewById(R.id.btn_next);
+        previous=(Button) findViewById(R.id.btn_previous);
+        music= MediaPlayer.create(Home_Activity.this,R.raw.the_place_inside);
+    }
+    private void processControl() {
+        play.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                music.start();
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                music.pause();
+            }
+        });
     }
 
     @Override
